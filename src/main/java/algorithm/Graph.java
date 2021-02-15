07014dc92ff264e9b860c7f177cbd0a805cdc010
apploid <apploidyakutsk@gmail.com>
2021-02-16 00:43:47 +0300

    use ArrayList with generic type instead of array

--- src/main/java/algorithm/Graph.java
+++ src/main/java/algorithm/Graph.java
@@ -2,24 +2,24 @@ package algorithm;
 import java.util.*;
 
 public class Graph {
-    private LinkedList<Integer> adjLists[];
-    private ArrayList<Integer> visitedVertices = new ArrayList<Integer>();
-    private boolean visited[];
+    private final ArrayList<LinkedList<Integer>> adjLists;
+    private final ArrayList<Integer> visitedVertices = new ArrayList<>();
+    private final boolean[] visited;
 
     public Graph(int vertices) {
-        adjLists = new LinkedList[vertices];
+        adjLists = new ArrayList<>();
         visited = new boolean[vertices];
 
         for (int i = 0; i < vertices; i++)
-            adjLists[i] = new LinkedList<Integer>();
+            adjLists.add(i, new LinkedList<Integer>());
     }
 
     public void addEdge(int src, int dest) {
-        adjLists[src].add(dest);
+        adjLists.get(src).add(dest);
     }
 
     public LinkedList<Integer> getEdges(int src) {
-        return adjLists[src];
+        return adjLists.get(src);
     }
 
     public ArrayList<Integer> getVisitedVertices() {
@@ -31,7 +31,7 @@ public class Graph {
         visitedVertices.add(vertex);
         System.out.print(vertex + " ");
 
-        for (int next : adjLists[vertex]){
+        for (int next : adjLists.get(vertex)){
             if (!visited[next]){
                 DFS(next);
             }
